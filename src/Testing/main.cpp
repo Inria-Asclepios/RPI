799b5480ff2129a2c53f74d3496dfc566cd42efd
John Stark <John.Stark@inria.fr>
2011-11-24 14:25:39 +0100

    Filename argument is optional.

--- src/Testing/main.cpp
+++ src/Testing/main.cpp
@@ -10,16 +10,19 @@ int main(int argc, char * argv[])
     //const std::string inputFilename = "c:/Temp/brebix.mha";
     //const std::string inputFilename = "c:/Temp/testfile.mha";
     //const std::string inputFilename = "C:/Users/jstark/Projects/sampleData/AMC_2011-11/Ex365Em505T12000ms.nii";
-    if (argc != 2 ) {
+    if (argc > 2 ) {
         std::cerr << "Usage " << argv[0] << " filename" <<std::endl;
         return 1;
     }
-    const std::string inputFilename = argv[1]; //"C:/Users/jstark/Projects/sampleData/AMC_2011-11/pig in ii.nii";
 
     MainWindow mw;
 
     mw.show();
 
+    if ( argc == 2 ) {
+        const QString inputFilename = argv[1]; //"C:/Users/jstark/Projects/sampleData/AMC_2011-11/pig in ii.nii";
+        mw.setFilename(inputFilename);
+    }
     return theApp.exec();
 
 }
