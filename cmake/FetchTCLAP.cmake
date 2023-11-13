d62e62e0a3f49940c146f78d5b6608ed96b64b3e
FLORENT LERAY <florent.leray@inria.fr>
2023-11-13 15:00:20 +0100

    Fix RPIModuleLocation.cmake bad install location

--- cmake/FetchTCLAP.cmake
+++ cmake/FetchTCLAP.cmake
@@ -41,25 +41,27 @@
 
 option(TCLAP_FETCH "Fetch TCLAP directly on internet" ON)
 SET( TCLAP_FOUND FALSE )
-FIND_PATH( TCLAP_INCLUDE_DIR tclap/CmdLine.h ${TCLAP_INCLUDE_DIR} ${CMAKE_BINARY_DIR}/_deps/tclap-src/include tclap/CmdLine.h /usr/include /opt/local/include )
-SET(TCLAP_INCLUDE_DIR "${TCLAP_INCLUDE_DIR}" CACHE PATH "" FORCE)
+IF (${TCLAP_FECTH})
+  FIND_PATH( TCLAP_INCLUDE_DIR tclap/CmdLine.h ${TCLAP_INCLUDE_DIR} ${CMAKE_BINARY_DIR}/_deps/tclap-src/include tclap/CmdLine.h /usr/include /opt/local/include )
+  SET(TCLAP_INCLUDE_DIR "${TCLAP_INCLUDE_DIR}" CACHE PATH "" FORCE)
 
-IF( TCLAP_INCLUDE_DIR )
-    SET( TCLAP_FOUND TRUE )
-ELSE() 
-    IF( ${TCLAP_FETCH} )        
-        include(FetchContent)
-        FetchContent_Declare(
-            tclap
-            URL https://netix.dl.sourceforge.net/project/tclap/tclap-1.2.1.tar.gz
-            URL_HASH MD5=eb0521d029bf3b1cc0dcaa7e42abf82a
-            )            
-    FetchContent_MakeAvailable(tclap)
-    SET(TCLAP_INCLUDE_DIR ${CMAKE_BINARY_DIR}/_deps/tclap-src/include CACHE PATH "" FORCE)
-    SET( TCLAP_FOUND TRUE )
-    ELSE()
-       MESSAGE( FATAL_ERROR "TCLAP was not found." )     
-    ENDIF()   
+  IF( TCLAP_INCLUDE_DIR )
+      SET( TCLAP_FOUND TRUE )
+  ELSE() 
+      IF( ${TCLAP_FETCH} )        
+          include(FetchContent)
+          FetchContent_Declare(
+              tclap
+              URL https://netix.dl.sourceforge.net/project/tclap/tclap-1.2.1.tar.gz
+              URL_HASH MD5=eb0521d029bf3b1cc0dcaa7e42abf82a
+              )            
+      FetchContent_MakeAvailable(tclap)
+      SET(TCLAP_INCLUDE_DIR ${CMAKE_BINARY_DIR}/_deps/tclap-src/include CACHE PATH "" FORCE)
+      SET( TCLAP_FOUND TRUE )
+      ELSE()
+         MESSAGE( FATAL_ERROR "TCLAP was not found." )     
+      ENDIF()   
+  ENDIF()
 ENDIF()
 
 IF( TCLAP_FOUND )
